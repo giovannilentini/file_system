0cbef463c3b598d065d5e9107d8933d9e165d7a2
giovannilentini <giovannilentini29@gmail.com>
2024-06-26 20:44:34 +0200

    update fs.sh

--- fs.sh
+++ fs.sh
@@ -4,7 +4,7 @@ test1() {
     if ! ./bin/dir_test; then
         handle_error
     else
-        echo "\n\033[1;32mTest 1 completed: Directory and file creation successfully executed.\033[0m\n"
+        echo '\n\033[1;32mTest 1 completed: Directory and file creation successfully executed.\033[0m\n'
     fi
 }
 
@@ -12,7 +12,7 @@ test2() {
     if ! ./bin/write_read_test; then
         handle_error
     else
-        echo "\n\033[1;32mTest 2 completed: File write and read successfully executed.\033[0m\n"
+        echo '\n\033[1;32mTest 2 completed: File write and read successfully executed.\033[0m\n'
     fi
 }
 
@@ -20,7 +20,7 @@ test3() {
     if ! ./bin/seek_test; then
         handle_error
     else
-        echo "\n\033[1;32mTest 3 completed: File write and read with seek successfully executed.\033[0m\n"
+        echo '\n\033[1;32mTest 3 completed: File write and read with seek successfully executed.\033[0m\n'
     fi
 }
 
@@ -28,7 +28,7 @@ test4() {
     if ! ./bin/erase_test; then
         handle_error
     else
-        echo "\n\033[1;32mTest 4 completed: Directory and file deletion successfully executed.\033[0m\n"
+        echo '\n\033[1;32mTest 4 completed: Directory and file deletion successfully executed.\033[0m\n'
     fi
 }
 
@@ -36,56 +36,55 @@ test5() {
     if ! ./bin/dir_test || ! ./bin/write_read_test || ! ./bin/seek_test || ! ./bin/erase_test; then
         handle_error
     else
-        echo "\n\033[1;32mAll tests successfully completed.\033[0m\n"
+        echo '\n\033[1;32mAll tests successfully completed.\033[0m\n'
     fi
 }
 
 handle_error() {
-    echo "\n\033[1;31mError: File not found.\033[0m"
-    echo "\nAre you sure you have compiled the files?"
+    echo '\n\033[1;31mError: File not found.\033[0m'
+    echo '\nAre you sure you have compiled the files?'
     echo "1. Yes, I have compiled"
     echo "2. No, I have not compiled"
     read -p "Enter your choice: " compile_choice
 
     case $compile_choice in
         1)
-
-        echo " _______________________________________________"
-        echo "/                                               \\"
-        echo "| \033[1;31m                  Don't Lie !!!           \033[0m    |"
-        echo "\\_________              ________________________/"
-        echo "          \\_         __/    ___---------__       "
-        echo "            \\      _/      /              \\     "
-        echo "             \\    /       /                 \\    "
-        echo "              |  /       | _    _ \\          \\   "
-        echo "              | |       / / \\  / \\ |          \\  "
-        echo "              | |       ||   ||   ||           | "
-        echo "              | |       | \\_//\\\\_/ |           |"
-        echo "              | |       |_| (||)   |_______|   |"
-        echo "              | |         |  ||     | _  / /   |"
-        echo "               \\ \\        |_________|| \\/ /   /"
-        echo "                \\ \\_       |_|_|_|_|/|  _/___/ "
-        echo "                 \\__>       _ _/_ _ /  |       "
-        echo "                          .|_|_|_|_|   |       "
-        echo "                          |           /        "
-        echo "                          |__________/         "
-
-        exit 1
-        ;;
+            echo '  ______________________________________________'
+            echo ' /                                              \\'
+            echo ' |\033[1;31m                  Don'"'"'t Lie !!!           \033[0m    |'
+            echo ' \\________              ________________________/'
+            echo '          \\_         __/    ___---------__       '
+            echo '            \\      _/      /              \\     '
+            echo '             \\    /       /                 \\    '
+            echo '              |  /       | _    _ \\          \\   '
+            echo '              | |       / / \\  / \\ |          \\  '
+            echo '              | |       ||   ||   ||           | '
+            echo '              | |       | \\_//\\\\_/ |           |'
+            echo '              | |       |_| (||)   |_______|   |'
+            echo '              | |         |  ||     | _  / /   |'
+            echo '               \\ \\        |_________|| \\/ /   /'
+            echo '                \\ \\_       |_|_|_|_|/|  _/___/ '
+            echo '                 \\__>       _ _/_ _ /  |       '
+            echo '                          .|_|_|_|_|   |       '
+            echo '                          |           /        '
+            echo '                          |__________/         '
+            echo ''
+            exit 1
+            ;;
         2)
-        echo "\nRunning make...\n"
-        make
-        ;;
+            echo '\nRunning make...\n'
+            make
+            ;;
         *)
-        echo "\n\033[1;31mInvalid choice. Exiting.\033[0m\n"
-        exit 1
-        ;;
-  esac
+            echo '\n\033[1;31mInvalid choice. Exiting.\033[0m\n'
+            exit 1
+            ;;
+    esac
 }
 
 test() {
     while true; do
-        echo "\nWhich test do you want to execute?"
+        echo '\nWhich test do you want to execute?'
         echo "1. Directory and file creation (recommended first)"
         echo "2. File write and read (recommended only if test 1 has been executed)"
         echo "3. File write and read with seek (recommended only if test 1 has been executed)"
@@ -95,50 +94,32 @@ test() {
         read -p "Enter your choice: " test_choice
 
         case $test_choice in
-            1)
-            test1
-            ;;
-            2)
-            test2
-            ;;
-            3)
-            test3
-            ;;
-            4)
-            test4
-            ;;
-            5)
-            test5
-            ;;
+            1) test1 ;;
+            2) test2 ;;
+            3) test3 ;;
+            4) test4 ;;
+            5) test5 ;;
             q)
-            echo "\nQuitting...\n"
-            break
-            ;;
-            *)
-            echo "\n\033[1;31mInvalid choice. Please try again.\033[0m\n"
-            ;;
+                echo $'\nQuitting...\n'
+                break
+                ;;
+            *) echo '\n\033[1;31mInvalid choice. Please try again.\033[0m\n' ;;
         esac
     done
 }
 
 shell() {
     clear
-  ./shell/main_shell
+    ./shell/main_shell
 }
 
-echo "\nWhat do you want to do?"
+echo '\nWhat do you want to do?'
 echo "1. View the tests"
 echo "2. Start the shell"
 read -p "Enter your choice: " main_choice
 
 case $main_choice in
-    1)
-        test
-        ;;
-    2)  
-        shell
-        ;;
-    *)
-        echo "\n\033[1;31mInvalid choice. Exiting.\033[0m\n"
-        ;;
+    1) test ;;
+    2) shell ;;
+    *) echo $'\n\033[1;31mInvalid choice. Exiting.\033[0m\n' ;;
 esac
